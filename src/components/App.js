7e7a8bd197f66baed1154b03c3c0fccb5cd07bb4
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-30 15:01:09 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -87,7 +87,7 @@ const App = () => {
                   <h2>{tour.name}</h2>
                   <div className="tour-info">
                     <p id={`tour-item-para-${tour.id}`}>{showFulltext ? tour.info : tour.info.substring(0, 200)}</p>
-                    <button onClick={handleShowMore}>Show more</button>
+                    <button onClick={handleShowMore} id={`see-more-${tour.id}`}>Show more</button>
                     <button onClick={handleShowLess}>Show less</button>
                   </div>
                   <img src={tour.image} width={"300px"}/>
